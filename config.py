9e7a7f9888ca06d2f81ff564e81266a98f195293
Ruiying-Ma <ruiying021120@gmail.com>
2026-01-13 00:52:14 -0800

    cleaned

--- config.py
+++ config.py
@@ -6,28 +6,40 @@ NODE_EMBEDDING_MODEL_LIST = ["sbert", "dpr", "bm25", "te3small"]
 SUMMARIZATION_MODEL = "gpt-4o-mini"
 DISTANCE_METRIC = "cosine"
 CONTEXT_LEN_RATIO_LIST = [0.05, 0.1, 0.15, 0.2, 0.3, 0.4]
-# CONTEXT_LEN_RATIO_LIST = [0.1, 0.2, 0.3, 0.4]
 DATASET_LIST = ["civic", "contract", "qasper", "finance"]
 SHT_TYPE_LIST = ["grobid", "intrinsic"]
 RAG_METHOD_LIST = ["sht", "raptor", "vanilla"]
 DATA_ROOT_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
-
-# config: the tuple that determines the context.jsonl
-# config: method, sht_type, node_embedding_model, embed_hierarchy, context_hierarchy, use_raw_chunks
 CONTEXT_CONFIG_LIST = (
     # end-to-end
-    # [("vanilla", None, nem, None, None, None, r) for nem in NODE_EMBEDDING_MODEL_LIST for r in CONTEXT_LEN_RATIO_LIST] + 
-    # [("raptor", None, nem, None, None, None, r) for nem in NODE_EMBEDDING_MODEL_LIST for r in CONTEXT_LEN_RATIO_LIST] + 
-    # [("sht", None, nem, True, True, True, r) for nem in NODE_EMBEDDING_MODEL_LIST for r in CONTEXT_LEN_RATIO_LIST] + 
-    # ablation on SHT
-    [("sht", sht_type, "sbert", True, True, True, 0.2) for sht_type in SHT_TYPE_LIST] + 
-    # ablation on HI
-    [("sht", None, "sbert", False, True, True, 0.2), ("sht", None, "sbert", True, False, True, 0.2), ("sht", None, "sbert", False, False, True, 0.2)] +
-    # ablation on CI
+    [("graphrag", None, None, None, None, None, None)] + 
+    [("hipporag", None, "sbert", None, None, None, r) for r in CONTEXT_LEN_RATIO_LIST] + 
+    [("vanilla", None, "sbert", None, None, None, r) for r in CONTEXT_LEN_RATIO_LIST] + 
+    [("raptor", None, "sbert", None, None, None, r) for r in CONTEXT_LEN_RATIO_LIST] + 
+    [("sht", None, "sbert", True, True, True, r) for r in CONTEXT_LEN_RATIO_LIST] +
+    # [("hipporag", None, "sbert", None, None, None, 0.4)] + 
+    # [("vanilla", None, "sbert", None, None, None, 0.4)] + 
+    # [("raptor", None, "sbert", None, None, None, 0.4)] + 
+    # [("sht", None, "sbert", True, True, True, 0.4)]
+    ## ablation on embedding models
+    [("hipporag", None, nem, None, None, None, 0.2) for nem in NODE_EMBEDDING_MODEL_LIST if nem != "sbert"] + 
+    [("vanilla", None, nem, None, None, None, 0.2) for nem in NODE_EMBEDDING_MODEL_LIST if nem != "sbert"] + 
+    [("raptor", None, nem, None, None, None, 0.2) for nem in NODE_EMBEDDING_MODEL_LIST if nem != "sbert"] + 
+    [("sht", None, nem, True, True, True, 0.2) for nem in NODE_EMBEDDING_MODEL_LIST if nem != "sbert"] + 
+    # # # ablation on SHT
+    [("sht", "intrinsic", "sbert", True, True, True, 0.2)] +
+    # [("sht", "grobid", "sbert", True, True, True, 0.2)] + 
+    [("sht", "wide", "sbert", True, True, True, 0.2)] + 
+    [("sht", "deep", "sbert", True, True, True, 0.2)] + 
+    [("sht", "llm_txt", "sbert", True, True, True, 0.2)] + 
+    [("sht", "llm_vision", "sbert", True, True, True, 0.2)] +
+    # [("sht", None, "sbert", True, True, True, 0.2)] +
+    # # # ablation on HI
+    [("sht", None, "sbert", False, True, True, 0.2), ("sht", None, "sbert", True, False, True, 0.2), ("sht", None, "sbert", False, False, True, 0.2)] + 
+    # # # ablation on CI
     [("sht", None, "sbert", True, True, False, 0.2)]
 )
 
-# print(len(CONTEXT_CONFIG_LIST))
 
 # config: the tuple that determines the index.jsonl
 # config: method, sht_type, node_embedding_model, embed_hierarchy
@@ -35,7 +47,6 @@ INDEX_CONFIG_LIST = sorted(list(set([
     tuple([i for i in config[:4]])
     for config in CONTEXT_CONFIG_LIST
 ])), key=lambda t: str(t))
-# print("\n".join([str(t) for t in INDEX_CONFIG_LIST]))
 
 
 def context_config_to_index_config(context_config):
@@ -45,6 +56,8 @@ def context_config_to_index_config(context_config):
 def get_index_jsonl_path(dataset, index_config_tuple):
     # config: method, sht_type, node_embedding_model, embed_hierarchy
     method, sht_type, node_embedding_model, embed_hierarchy = index_config_tuple
+    if method in ["graphrag", "hipporag"]:
+        raise ValueError(f"{method} doesn't have index jsonl file!")
 
     query_embedding_model = node_embedding_model
 
@@ -71,6 +84,12 @@ def get_index_jsonl_path(dataset, index_config_tuple):
     return index_jsonl_path
 
 def get_config_jsonl_path(dataset, context_config_tuple):
+    if context_config_tuple[0] == "hipporag":
+        context_len_ratio = context_config_tuple[-1]
+        node_embedding_model = context_config_tuple[2]
+        context_jsonl_path = os.path.join(DATA_ROOT_FOLDER, dataset, "baselines", f"{node_embedding_model}.{SUMMARIZATION_MODEL}.c{CHUNK_SIZE}.s{SUMMARY_LEN}", f"{node_embedding_model}.{DISTANCE_METRIC}.hipporag", "o0", f"context{context_len_ratio}", "context.jsonl")
+        return context_jsonl_path
+    
     index_config_tuple = context_config_to_index_config(context_config_tuple)
     index_jsonl_path = get_index_jsonl_path(dataset, index_config_tuple)
     context_folder = os.path.dirname(index_jsonl_path)
